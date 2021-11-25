575a4c60b5eb2426891f28b6b08615cd3c6954b4
jollypolly123 <rachelle.hu123@gmail.com>
2021-11-25 15:04:55 -0500

    create api call functions

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
 import './App.css';
 import React, { useState } from "react";
+import { checkStatus, listTodos, createTodo, retrieveTodo, updateTodo, deleteTodo, toggleTodo } from "./apiCalls";
 
 function App() {
   const [todoList, updateTodo] = useState(["Cras justo odio", "Dapibus ac facilisis in"]);
   const [page, switchPage] = useState(0);
+
+  checkStatus().then(r => console.log(r));
+  listTodos().then(r => console.log(r));
   
   return (
     <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center pt-4">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-center pt-4">
         <h1>
           WaffleHacks To do list
         </h1>
       </nav>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark text-center justify-content-center">
-        <ul class="navbar-nav mr-auto text-center">
-          <li class={`nav-item ${page == 0 ? "active": ""}`}>
-            <span class="nav-link" onClick={() => switchPage(0)}>In Progress</span>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-center justify-content-center">
+        <ul className="navbar-nav mr-auto text-center">
+          <li className={`nav-item ${page === 0 ? "active": ""}`}>
+            <span className="nav-link" onClick={() => switchPage(0)}>In Progress</span>
           </li>
-          <li class={`nav-item ${page == 1 ? "active": ""}`}>
-            <span class="nav-link" onClick={() => switchPage(1)}>Completed</span>
+          <li className={`nav-item ${page === 1 ? "active": ""}`}>
+            <span className="nav-link" onClick={() => switchPage(1)}>Completed</span>
           </li>
         </ul>
-        <form class="form-inline">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search for a task" aria-label="Search" />
-          <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+        <form className="form-inline">
+          <input className="form-control mr-sm-2" type="search" placeholder="Search for a task" aria-label="Search" />
+          <button className="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
         </form>
       </nav>
-      <ul class="list-group py-3">
+
+      <ul className="list-group py-3">
         {todoList.map(item => {
-          return (<li class="list-group-item">{item}</li>)
+          return (<li className="list-group-item">{item}</li>)
         })}
       </ul>
+
     </div>
   );
 }
